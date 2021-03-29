e9a0df4c0ff181377654ed8f1cae1954d4bccd9d
Medeirosseth <79333650+Medeirosseth@users.noreply.github.com>
2021-03-29 14:33:01 -0700

    WIP trouble shooting append
    Co-authored-by Giancarlo Vigneri <bobloblaw.vigneri@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,30 +1,50 @@
-let favorite = [];
-let user1 = [];
+
+
+
 $(document).ready(function() {
-  
+  let favorite = [];
+  let user1 = [];
   console.log(favorite)
   $(".formOne").submit(function(event) {
     event.preventDefault();
     const icecream = $("input#icecream").val();
     const food = $("input#food").val();
     const color = $("input#color").val();
+ 
     console.log(food)
     favorite = [icecream, food, color];
-    //user1= favorite.push(user1)// adds 4 to favorite
-    user1= user1.push(favorite)//returns 1 in alert
+    
+    user1.push(favorite [0], favorite[1])//inefficient solution
 
-    alert(favorite);//testing favorite value within scope
-    alert(user1);
-
+    //alert(favorite);//testing favorite value within scope
+    //alert(user1);
 
     const result= icecream + ", " + food + ", " + color;
     $("#result").text(result);
     
+    $("ul").append('<li>'+user1[0]+'</li>');
+    $("ul").append('<li>'+user1[1]+'</li>'); 
     
-    //const favorite = ()
-    //favorite.push(icecream)
-  
-  }); 
- 
-  
+    
+    //$("button#submit").click(function() {
+   
+     // $("ul").append('<li>'+user1[0]+'</li>');
+     // $("ul").append('<li>'+user1[1]+'</li>'); 
+   // });  
+
+  })  
 })
+
+//$('#content ul').append(
+  //$('<li>').append(
+    //  $('<a>').attr('href','/user/messages').append(
+      //    $('<span>').attr('class', 'tab').append("Message center")
+
+
+        //  $("ul").append(
+          //  $('<li>').append(
+            ////  $(user1).append(
+                //$('<li>')
+                
+             // )
+           // )     
